4fb06c61db88cd5db607041c03159500fe702fa9
Fredricky12 <fredrickcuche@gmail.com>
2024-05-01 08:22:09 +0100

    record page

--- website/views.py
+++ website/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
 def home(request):
+    records = Record.objects.all()
+
+
     #check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +25,16 @@ def home(request):
             messages.success(request, "Thers was an Error Logging in please try again...")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
+    
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk )
+        return(request, 'record.html', {'customer_record':customer_record})
+    else: 
+        messages.success(request, "You must be logged in!")
+        return redirect('home')
     
 
     
@@ -33,4 +47,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+			# Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+
+    return render(request, 'register.html', {'form':form})
